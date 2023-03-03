a86bd3f788142a5470beece1982acfc66c7bea29
Naomi Ainspan <nainspan@gmail.com>
2023-03-03 16:00:19 -0500

    inbox page

---
+++ src/pages/inbox.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { withLayout } from '../wrappers/layout';
+import { CometChatGroupListWithMessages } from '../CometChatWorkspace/src';
+const InboxPage = () => {
+  return (
+    <div className="h-screen w-full">
+      <CometChatGroupListWithMessages />
+    </div>
+  );
+};
+export default withLayout(InboxPage);
